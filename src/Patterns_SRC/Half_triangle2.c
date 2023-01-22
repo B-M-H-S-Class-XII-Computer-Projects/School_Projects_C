551a1875e1d5a2a33eedfd59e2962ea584f5527e
Jack Oriander <102282297+JackOriander@users.noreply.github.com>
2023-01-22 11:35:43 +0530

    Add files via upload

---
+++ src/Patterns_SRC/Half_triangle2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i=0; i<5; i++)
+    {
+        for(j=4; j>=i; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
---
+++ src/Patterns_SRC/Half_triangle4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i=5; i>0; i--)
+    {
+        for(j=0; j<i; j++)
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+}
